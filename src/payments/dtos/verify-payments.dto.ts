39b525f5497f823aaf66cc5adcf79af355a10d7e
Sage <tuzzy08@gmail.com>
2021-08-04 11:11:41 +0100

    Added Paystack payment verification endpoint

--- src/payments/dtos/verify-payments.dto.ts
+++ src/payments/dtos/verify-payments.dto.ts
@@ -3,9 +3,5 @@ import { IsNotEmpty, IsString } from 'class-validator';
 export class VerifyPaymentDto {
   @IsNotEmpty()
   @IsString()
-  tx_ref: string;
-
-  @IsNotEmpty()
-  @IsString()
-  transaction_id: string;
+  reference: string;
 }
